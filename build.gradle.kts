5a9fe5ad6c4a9e61bc001c84eea7636153d9806a
Dmitry Titov <ditriy.titov@mail.ru>
2023-05-19 16:11:03 +0500

    New project

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+   // kotlin("jvm") version "1.8.0"
+    id("org.jetbrains.kotlin.jvm") version "1.8.21"
+    application
+}
+
+group = "ru.netology"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
